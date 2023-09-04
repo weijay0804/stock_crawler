ca7005b9a3d085c661b39f7b57923c93a6f1cb7a
weijay <weijay0804@gmail.com>
2023-09-05 00:24:28 +0800

    fix: 重新調整資料格式，並新增更詳細的說明

--- main.py
+++ main.py
@@ -30,6 +30,14 @@ class StatementDog:
         取得市場焦點的前三名和後三名的產業類別
 
         args: 要取得的天數 (1day, 1week, 1month, 3months)
+
+        i.e:
+        ```
+        {
+        "top" : [{"name" : "砷化鎵", "diff_percentage" : 3.87, "url" : "http...."}, ...],
+        "last" : [{"name" : "家電", "diff_percentage" : -8.23, "url" : "http...."}, ...]
+        }
+        ```
         """
 
         response = BaseRequset.get_requset(f"https://statementdog.com/api/v1/market-trend/tw/{arg}")
@@ -55,9 +63,17 @@ class StatementDog:
 
         url: 產業詳細頁面 url
         name: 產業名稱
+
+        i.e:
+        ```
+        {
+        "group" : "砷化鎵",
+        "data" : [["3105", "穩懋"], ["2455", "全新"], ["8086", "宏捷科"]]
+        }
+        ```
         """
 
-        response = BaseRequset.get_requset(url)
+        response = BaseRequset.get_requset(f"{url}?country=tw")
 
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -70,18 +86,14 @@ class StatementDog:
         for item in items:
             code_name = item.text.replace("\n", "").split(" ")
 
-            try:
-                code_name[0] = int(code_name[0])
-                stock_list.append(code_name)
-
-            except ValueError:
-                continue
+            stock_list.append(code_name)
 
         stock_list = stock_list[:3]
 
         result = {}
 
-        result[name] = stock_list
+        result["group"] = name
+        result["data"] = stock_list
 
         return result
 
@@ -91,6 +103,22 @@ class StatementDog:
         取得市場焦點和每個產業的股票
 
         args: 要取得的天數 (1day, 1week, 1month, 3months)
+
+        i.e:
+        ```
+        {
+        "top" : [
+            {
+                "group" : "砷化鎵",
+                "data" : [["3105" : "穩懋"], ...]
+            }, ...],
+        "last" : [
+            {
+                "group" : "LCD塑膠框",
+                "data" : [["2371" : "大同"]. ...]
+            }, ...]
+        }
+        ```
         """
 
         result = defaultdict(list)
@@ -118,18 +146,19 @@ class StockPrice:
         (代碼、名稱、開高低收)
         """
 
-        result = []
+        result = {}
 
         for data in stock_data:
             tmp = {
-                data["Code"]: data["Name"],
-                "opening_price": data["OpeningPrice"],
-                "highest_price": data["HighestPrice"],
-                "lowest_price": data["LowestPrice"],
-                "cloesing_price": data["ClosingPrice"],
+                "code": data["Code"],
+                "name": data["Name"],
+                "opening_price": float(data["OpeningPrice"]) if data["OpeningPrice"] else None,
+                "highest_price": float(data["HighestPrice"]) if data["HighestPrice"] else None,
+                "lowest_price": float(data["LowestPrice"]) if data["LowestPrice"] else None,
+                "cloesing_price": float(data["ClosingPrice"]) if data["ClosingPrice"] else None,
             }
 
-            result.append(tmp)
+            result[data["Code"]] = tmp
 
         return result
 
@@ -140,18 +169,19 @@ class StockPrice:
         (代碼、名稱、開高低收)
         """
 
-        result = []
+        result = {}
 
         for data in mainborad_data:
             tmp = {
-                data["SecuritiesCompanyCode"]: data["CompanyName"],
-                "opening_price": data["Open"],
-                "highest_price": data["High"],
-                "lowest_price": data["Low"],
-                "cloesing_price": data["Close"],
+                "code": data["SecuritiesCompanyCode"],
+                "name": data["CompanyName"],
+                "opening_price": float(data["Open"]) if data["Open"] != "----" else None,
+                "highest_price": float(data["High"]) if data["High"] != "----" else None,
+                "lowest_price": float(data["Low"]) if data["Low"] != "----" else None,
+                "cloesing_price": float(data["Close"]) if data["Close"] != "----" else None,
             }
 
-            result.append(tmp)
+            result[data["SecuritiesCompanyCode"]] = tmp
 
         return result
 
@@ -159,6 +189,19 @@ class StockPrice:
     def get_stock_day_all() -> dict:
         """
         根據證交所的 API 取得上市股票的每日交易資料
+
+        i.e:
+        ```
+        {
+        "3105" : {
+        "code" : "3105",
+        "name" : "穩懋",
+        "opening_price" : 141.0,
+        "highest_price" : 146.0,
+        "lowest_price" : 139.0,
+        "cloesing_price" : 144.0}, ...
+        }
+        ```
         """
 
         response = BaseRequset.get_requset(
@@ -171,6 +214,19 @@ class StockPrice:
     def get_mainborad_day_all() -> dict:
         """
         根據櫃買中心的 API 取得上櫃股票的每日交易資訊
+
+        i.e:
+        ```
+        {
+        "3105" : {
+        "code" : "3105",
+        "name" : "穩懋",
+        "opening_price" : 141.0,
+        "highest_price" : 146.0,
+        "lowest_price" : 139.0,
+        "cloesing_price" : 144.0}, ...
+        }
+        ```
         """
 
         response = BaseRequset.get_requset(
