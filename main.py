67dcbca84336bb6bfede184397567dd2ac18d5e9
weijay <weijay0804@gmail.com>
2023-09-05 16:28:57 +0800

    feat: 完成 財報狗功能

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 from bs4 import BeautifulSoup
 import requests
+import openpyxl
 
 
 class BaseRequset:
@@ -46,8 +47,8 @@ class StatementDog:
 
         sorted_datas = sorted(datas, key=lambda i: i["diff_percentage"], reverse=True)
 
-        top = sorted_datas[:3]
-        last = sorted_datas[-1:-4:-1]
+        top = sorted_datas[:5]
+        last = sorted_datas[-1:-6:-1]
 
         result = {}
 
@@ -85,6 +86,9 @@ class StatementDog:
 
         for item in items:
             code_name = item.text.replace("\n", "").split(" ")
+
+            if len(code_name) > 2:
+                code_name = [code_name[0], "".join(code_name[1:])]
 
             stock_list.append(code_name)
 
@@ -130,6 +134,50 @@ class StatementDog:
                 top_last_3 = StatementDog.get_group_top_last_3(data["url"], data["name"])
 
                 result[i].append(top_last_3)
+
+        return result
+
+    @staticmethod
+    def get_final_data(
+        stock_price_data: dict, mainborad_price_data: dict, arg: str = "1day"
+    ) -> dict:
+        """
+        取得最終處理完的資料，換句話說就是取得開高低收等資訊
+
+        args: 要取得的天數 (1day, 1week, 1month, 3months)
+
+        i.e:
+        ```
+        {
+        "top" : [
+            {"group" : "砷化鎵", "data" : [
+                {"code" : "3105", "name" : "穩懋", "opening_price" : 101.1, "highest_price" : 120.0, "lowest_price" : 100.0, "cloesing_price" : 102.2}, ...]
+            }, ...],
+        "last: [....]
+        }
+        ```
+        """
+
+        result = defaultdict(list)
+        meta_data = StatementDog.get_data(arg)
+
+        for k in meta_data:
+            for group_data in meta_data[k]:
+                tmp = {}
+                tmp["group"] = group_data["group"]
+                tmp["data"] = []
+
+                for stock in group_data["data"]:
+                    if stock_price_data.get(stock[0]):
+                        tmp["data"].append(stock_price_data[stock[0]])
+
+                    elif mainborad_price_data.get(stock[0]):
+                        tmp["data"].append(mainborad_price_data[stock[0]])
+
+                    else:
+                        tmp["data"].append(None)
+
+                result[k].append(tmp)
 
         return result
 
@@ -204,6 +252,7 @@ class StockPrice:
         ```
         """
 
+        # TODO 要改成用 https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL 這邊的資料會比較準確
         response = BaseRequset.get_requset(
             "https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL"
         )
@@ -234,3 +283,216 @@ class StockPrice:
         )
 
         return StockPrice._translate_mainborad_data(response.json())
+
+
+class ExcelWriter:
+    """
+    負責處理將資料寫入 excel 的類別
+    """
+
+    def __init__(self, base_filename: str, save_filename: str):
+        """
+
+        base_filename (str): 基本 excel 模板檔案路徑
+        save_filename (str): 要儲存的 excel 檔案路徑
+        """
+
+        self.wb = openpyxl.load_workbook(base_filename)
+        self.save_name = save_filename
+
+    def _write_data(
+        self,
+        data: dict,
+        group_number: int,
+        worksheet_name: str,
+        group_col_code: str,
+        data_col_start: str,
+        data_col_end: str,
+    ):
+        """將資料寫入至 excel 中
+
+        Args:
+            data (dict): 要寫入的資料
+            group_number (int): 族群的數量
+            worksheet_name (str): 要寫入至哪個分頁的名稱
+            group_col_code (str): 族群名稱的欄位代號 i.e: "A"
+            data_col_start (str): 資料範圍的開始欄位代號 i.e: "C"
+            data_col_end (str): 資料範圍的結束欄位代號 i.e: "H"
+
+        data 的格式要符合：
+        ```
+        [
+            {
+                "group" : "砷化鎵",
+                "data" : [
+                    {
+                        "code" : "3105",
+                        "name" : "穩懋",
+                        "opening_price" : 141.0,
+                        "highest_price" : 146.0,
+                        "lowest_price" : 139.0,
+                        "cloesing_price" : 144.0
+                    }, ...]
+            }, ....
+        ]
+        ```
+        """
+
+        s1 = self.wb[worksheet_name]
+
+        # 控制族群名稱
+        for i in range(group_number):
+            # 寫入族群名稱
+            s1[f"{group_col_code}{6 + (i*3)}"].value = data[i]["group"]
+
+            # 控制每個族群中的三筆資料
+            for j in range(3):
+                n = i * 3 + j
+
+                cell_range = s1[f"{data_col_start}{6 + n}":f"{data_col_end}{6 + n}"]
+
+                stcok_data = data[i]["data"][j]
+
+                # 寫入各項資料
+                cell_range[0][0].value = stcok_data["code"] if stcok_data["code"] else "null"
+                cell_range[0][1].value = stcok_data["name"] if stcok_data["name"] else "null"
+                cell_range[0][2].value = (
+                    stcok_data["opening_price"] if stcok_data["opening_price"] else "null"
+                )
+                cell_range[0][3].value = (
+                    stcok_data["highest_price"] if stcok_data["highest_price"] else "null"
+                )
+                cell_range[0][4].value = (
+                    stcok_data["lowest_price"] if stcok_data["lowest_price"] else "null"
+                )
+                cell_range[0][5].value = (
+                    stcok_data["cloesing_price"] if stcok_data["cloesing_price"] else "null"
+                )
+
+        self.wb.save(self.save_name)
+
+    def write_statement_dog_data(self, statement_dog_data: dict, day_type: str):
+        """將財報狗的資料寫入 execl
+
+        statement_dog_data (dict): 財報狗的資料，由 ``StatementDog.get_final_data()`` 生成的資料
+        day_type (str): 資料的種類 ("1day", "1week", "1month", "3months")
+        """
+
+        if day_type == "1day":
+            raise_group_col_code = "A"
+            raise_data_col_start_code = "C"
+            raise_data_col_end_code = "H"
+
+            reduce_group_col_code = "BA"
+            reduce_data_col_start_code = "BC"
+            recude_data_col_end_code = "BH"
+
+        elif day_type == "1week":
+            raise_group_col_code = "N"
+            raise_data_col_start_code = "P"
+            raise_data_col_end_code = "U"
+
+            reduce_group_col_code = "BN"
+            reduce_data_col_start_code = "BP"
+            recude_data_col_end_code = "BU"
+
+        elif day_type == "1month":
+            raise_group_col_code = "AA"
+            raise_data_col_start_code = "AC"
+            raise_data_col_end_code = "AH"
+
+            reduce_group_col_code = "CA"
+            reduce_data_col_start_code = "CC"
+            recude_data_col_end_code = "CH"
+
+        elif day_type == "3months":
+            raise_group_col_code = "AN"
+            raise_data_col_start_code = "AP"
+            raise_data_col_end_code = "AU"
+
+            reduce_group_col_code = "CN"
+            reduce_data_col_start_code = "CP"
+            recude_data_col_end_code = "CU"
+
+        else:
+            raise ValueError("Invalid value for 'day_type'")
+
+        self._write_data(
+            statement_dog_data["top"],
+            5,
+            "漲跌幅-前五族群前三檔",
+            raise_group_col_code,
+            raise_data_col_start_code,
+            raise_data_col_end_code,
+        )
+        self._write_data(
+            statement_dog_data["last"],
+            5,
+            "漲跌幅-前五族群前三檔",
+            reduce_group_col_code,
+            reduce_data_col_start_code,
+            recude_data_col_end_code,
+        )
+
+
+if __name__ == "__main__":
+    statement_dog = StatementDog()
+    stokc_price = StockPrice()
+    excel = ExcelWriter("./base.xlsx", "test4.xlsx")
+
+    print("取得股票交易資料....")
+    stokc_price_all_day = stokc_price.get_stock_day_all()
+    mainborad_price_all_day = stokc_price.get_mainborad_day_all()
+    print("處理完成")
+
+    print(f"{'-' * 5} 爬取財報狗資料 {'-' * 5}")
+
+    print("取得 [1day] 資料...")
+
+    statement_dog_1day = statement_dog.get_final_data(
+        stokc_price_all_day, mainborad_price_all_day, "1day"
+    )
+
+    print("爬取完成")
+
+    print("寫入 excel...")
+
+    excel.write_statement_dog_data(statement_dog_1day, "1day")
+
+    print("寫入完成")
+
+    print("取得 [1week] 資料...")
+    statement_dog_1week = statement_dog.get_final_data(
+        stokc_price_all_day, mainborad_price_all_day, "1week"
+    )
+    print("爬取完成")
+
+    print("寫入 execl...")
+    excel.write_statement_dog_data(statement_dog_1week, "1week")
+    print("寫入完成")
+
+    print("取得 [1month] 資料...")
+    statement_dog_1month = statement_dog.get_final_data(
+        stokc_price_all_day, mainborad_price_all_day, "1month"
+    )
+    print("爬取完成")
+
+    print("寫入 excel...")
+    excel.write_statement_dog_data(statement_dog_1month, "1month")
+    print("寫入完成")
+
+    print("取得 [3months] 資料...")
+    statement_dog_3months = statement_dog.get_final_data(
+        stokc_price_all_day, mainborad_price_all_day, "3months"
+    )
+
+    print("爬取完成")
+
+    print("寫入 excel...")
+    excel.write_statement_dog_data(statement_dog_3months, "3months")
+    print("寫入完成")
+
+    print(f"{'-' * 5} 財報狗資料處理完畢 {'-' * 5}")
+
+    excel.wb.close()
+    print("程式執行結束")
